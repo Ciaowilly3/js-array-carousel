ca665d11c4c2829502ecff4397dc41e449745d45
Ciaowilly3 <muscomassi@gmail.com>
2022-11-04 18:50:01 +0100

    themplate completo manca interazione

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const carousel = [
+    "imgs/01.webp",
+    "imgs/02.webp",
+    "imgs/03.webp",
+    "imgs/04.webp",
+    "imgs/05.webp",
+];
+
+const carouselEl = document.getElementById("carousel");
+
+let imgScroller = 2;
+
+carouselEl.src= carousel[imgScroller];
