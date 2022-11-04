e846f07c86b24cac55aa94c51b4965cd0f2e5608
Ciaowilly3 <muscomassi@gmail.com>
2022-11-04 18:59:49 +0100

    carousel funzionante

--- js/main.js
+++ js/main.js
@@ -8,6 +8,27 @@ const carousel = [
 
 const carouselEl = document.getElementById("carousel");
 
-let imgScroller = 2;
+let imgScroller = 0;
 
 carouselEl.src= carousel[imgScroller];
+
+const topBtnEl = document.querySelector(".top-btn");
+const btmBtnEl = document.querySelector(".btm-btn");
+
+console.log(topBtnEl);
+topBtnEl.addEventListener( "click", function(){
+    imgScroller++;
+    if(imgScroller>=carousel.length){
+        imgScroller = 0;
+    }
+    carouselEl.src = carousel[imgScroller];
+    console.log(imgScroller)
+} )
+btmBtnEl.addEventListener( "click", function(){
+    imgScroller--;
+    if(imgScroller<0){
+        imgScroller = 4;
+    }
+    carouselEl.src = carousel[imgScroller];
+    console.log(imgScroller)
+} )
